54e71d582f8cba0f8c178809b1a4e60a218cad55
PedroVasco03 <baltazarpedro092@gmail.com>
2023-10-22 16:47:49 +0100

    decisions

--- app.ts
+++ app.ts
@@ -1,26 +1,54 @@
-interface Pessoa{
-    nome : string,
-    idade : number,
-    profissao?: string
+// interface Pessoa{
+//     nome : string,
+//     idade : number,
+//     profissao?: string
+// }
+
+// const Pessoa : Pessoa={
+//     nome : 'Pedro',
+//     idade: 20,
+//     profissao:'Dev'
+// }
+
+// const Pessoa2: Pessoa = {
+//     nome: 'André',
+//     idade: 18
+// }
+
+// const arrayPessoa : Array<Pessoa> = [
+//     Pessoa,
+//     Pessoa2
+// ]
+
+// const arrayNum: number [] = [1, 2, 3, 4, 5]
+// const arrayString: Array<string> = [
+//     '1', '2', '3', '4', '5'
+// ]
+
+// const numn: number =15
+
+// if(numn > 15){
+//     console.log('Num maior que 15')
+// }
+// else if(numn === 15){
+//     console.log('Num igual a 15')
+// }
+// else{
+//     console.log('Num menor que 15')
+// }
+
+const typeUser = {
+    admin: 'Seja bem vindo administrator',
+    student: 'Você é estudante',
+    viewer: 'Você pode vizualizar'
 }
 
-const Pessoa : Pessoa={
-    nome : 'Pedro',
-    idade: 20,
-    profissao:'Dev'
+function validateUser(user: string){
+    console.log(typeUser[user as keyof typeof typeUser])
 }
 
-const Pessoa2: Pessoa = {
-    nome: 'André',
-    idade: 18
-}
+const usuario = 'admin'
 
-const arrayPessoa : Array<Pessoa> = [
-    Pessoa,
-    Pessoa2
-]
-
-const arrayNum: number [] = [1, 2, 3, 4, 5]
-const arrayString: Array<string> = [
-    '1', '2', '3', '4', '5'
-]
+validateUser(usuario)
+validateUser('student')
+validateUser('viewer')
