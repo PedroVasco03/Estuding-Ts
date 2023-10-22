e045a4a529511318c4adb1c0e1f00ee610d8f8a8
PedroVasco03 <baltazarpedro092@gmail.com>
2023-10-22 20:00:54 +0100

    class

--- app.ts
+++ app.ts
@@ -64,18 +64,40 @@
 // console.log(array[0])
 // console.log(strtringArray.length)
 
-class User {
-    name: string
-    age: number
+// class User {
+//     name: string
+//     age: number
 
-    constructor(name: string, age: number){
-        this.name=name;
-        this.age=age;
+//     constructor(name: string, age: number){
+//         this.name=name;
+//         this.age=age;
+//     }
+
+//     showName(){
+//         console.log(this.name)
+//     }
+// }
+// const user = new User('Pedro Vasco', 20)
+// user.showName();
+
+
+class Account{
+    name: string
+    accountNumber: number
+
+    constructor(name: string, accountNumber: number){
+        this.name = name
+        this.accountNumber = accountNumber
     }
 
-    showName(){
-        console.log(this.name)
+    deposit = () => {
+        console.log("Você Depositou")
+    }
+
+    withdraw = () => {
+        console.log("Você Sacou")
     }
 }
-const user = new User('Pedro Vasco', 20)
-user.showName();
+
+const newAccount :Account = new Account ('Pedro Vasco', 20)
+console.log(newAccount)
