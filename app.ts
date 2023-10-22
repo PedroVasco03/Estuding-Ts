1daf75a2d8930317da349f9eb467a936ab492c5b
PedroVasco03 <baltazarpedro092@gmail.com>
2023-10-22 19:40:42 +0100

    methods

--- app.ts
+++ app.ts
@@ -63,3 +63,19 @@
 
 // console.log(array[0])
 // console.log(strtringArray.length)
+
+class User {
+    name: string
+    age: number
+
+    constructor(name: string, age: number){
+        this.name=name;
+        this.age=age;
+    }
+
+    showName(){
+        console.log(this.name)
+    }
+}
+const user = new User('Pedro Vasco', 20)
+user.showName();
